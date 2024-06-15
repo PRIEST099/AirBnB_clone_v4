b4bef249c5a835dad4172254f8b03cf8ba2a2fe8
Ahadi <ahadic044@gmail.com>
2024-06-15 08:47:07 +0200

    [ADD] AirBnb dynamic content

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -9,8 +9,16 @@ $(document).ready(function () {
     } else {
       delete amenities[amenityId];
     }
-    const amenitiesList = Object.values(amenities).join(', ');
+    const amenitiesList = Object.values(amenities).join(' ');
     const $h_four = $('.amenities h4');
     $h_four.text(amenitiesList);
+  });
+
+  $.get('http://0.0.0.0:5001/api/v1/status/', data => {
+    if (data.status == 'OK') {
+      $('DIV#api_status').addClass('available');
+    } else {
+      $('DIV#api_status').removeClass('available');
+    }
   });
 });
